95c5512e1fc0e79155c5975e7a8cb5da1bf727ad
jenrose24 <rosienifer@icloud.com>
2018-07-13 21:58:07 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,2 +1,22 @@
 'use-strict';
 //add your code here
+function h1Selector(){
+  return $('h1')
+}
+
+function liInOlSelector(){
+  return $('ol li')
+}
+
+function linkSelector(){
+  return $('#box4.box5 a')
+}
+
+function imageSelector(){
+  return $('img[alt="cat sleeping"]')
+  //(img[alt="5 copy_200comp.jpg"])
+}
+
+function checkboxInputSelector(){
+return $('input[type="checkbox"]')
+}
